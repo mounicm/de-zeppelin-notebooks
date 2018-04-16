5bba46c578fcf4f48c690f91dffd0fbe52a7c99d
Mounic - GDP <mounicm@seekasia.com>
2018-04-17 00:49:15 +0800

    updated matplot

--- zeppelin.py
+++ zeppelin.py
@@ -11,3 +11,21 @@ print (z.select("f1",[("o1","1"),("o2","2")],"2"))
 
 ### Checkbox form
 print("".join(z.checkbox("f3", [("o1","1"), ("o2","2")],["1"])))
+
+'''
+Matplotlib integration
+The python interpreter can display matplotlib graph with the function z.show(). You need to have matplotlib module installed and a XServer running to use this functionality
+'''
+%python
+import matplotlib.pyplot as plt
+plt.figure()
+(.. ..)
+z.show(plt)
+plt.close()
+
+'''
+z.show function can take optional parameters to adapt graph width and height
+'''
+%python
+z.show(plt, width='50px')
+z.show(plt, height='150px')
